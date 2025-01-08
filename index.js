50545847ba77d68b88fb031e8cf72642ffe95bf1
Yash Gavali <yash16gavali@gmail.com>
2025-01-08 22:53:22 +0530

    express-sessions

--- index.js
+++ index.js
@@ -5,48 +5,63 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
 
-const listings=require("./routes/listing.js");
-const reviews=require("./routes/review.js");
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "/public")));
 
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly : true,
+  },
+};
+
+app.use(session(sessionOptions));
+
 const Mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("connected to DB");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 async function main() {
-    mongoose.connect(Mongo_url);
+  mongoose.connect(Mongo_url);
 }
 
 app.get("/", (req, res) => {
-    res.send("Hii! I am root!");
+  res.send("Hii! I am root!");
 });
-
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page not found!"));
-    // next(err);
+  next(new ExpressError(404, "Page not found!"));
+  // next(err);
 });
 
 app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "Something went wrong!" } = err;
-    // res.status(statusCode).send(message);
-    res.status(statusCode).render("listings/error.ejs", { message });
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  // res.status(statusCode).send(message);
+  res.status(statusCode).render("listings/error.ejs", { message });
 });
 
 app.listen(8081, () => {
-    console.log("Server is listening on port 8081");
+  console.log("Server is listening on port 8081");
 });
